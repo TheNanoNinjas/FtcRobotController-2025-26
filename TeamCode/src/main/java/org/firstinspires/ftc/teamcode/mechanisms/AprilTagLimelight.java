1164b853ef49f92571c4106d88adbdfdf07ef135
mahith <mahith.meruva2012@gmail.com>
2026-01-29 20:36:59 -0600

    comiting code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/AprilTagLimelight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/AprilTagLimelight.java
@@ -1,25 +1,57 @@
-package org.firstinspires.ftc.teamcode.mechanisms;
+    package org.firstinspires.ftc.teamcode.mechanisms;
 
-import org.firstinspires.ftc.teamcode.util.RobotHardware;
-import com.qualcomm.hardware.limelightvision.Limelight3A;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+    import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+    import org.firstinspires.ftc.teamcode.util.RobotHardware;
 
-public class AprilTagLimelight extends OpMode {
-   private Limelight3A Limelight;
+    import com.qualcomm.hardware.limelightvision.LLResult;
+    import com.qualcomm.hardware.limelightvision.Limelight3A;
+    import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 
-    @Override
-    public void init() {
-        Limelight = hardwareMap.get(Limelight3A.class, "Limelight");
-Limelight.pipelineSwitch(8);
+    import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+    import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+    import com.qualcomm.robotcore.hardware.IMU;
+
+    import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+    @Autonomous(name = "Auto April Tags", group = "Competition")
+    public class AprilTagLimelight extends OpMode {
+       private Limelight3A Limelight3A;
+
+       private IMU imu;
+       private double distance;
+
+        @Override
+        public void init() {
+            Limelight3A = hardwareMap.get(Limelight3A.class, "Limelight");
+    Limelight3A.pipelineSwitch(8);
+            imu = hardwareMap.get(IMU.class, "imu");
+
+            IMU.Parameters parameters = new IMU.Parameters(
+                    new RevHubOrientationOnRobot(
+                            RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
+                            RevHubOrientationOnRobot.UsbFacingDirection.UP 
+                    )
+            );
+            imu.initialize(parameters);
+
+        }
+
+        @Override
+        public void start(){
+    Limelight3A.start();
+
     }
+        @Override
+        public void loop() {
+            YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
+            Limelight3A.updateRobotOrientation(orientation.getYaw());
+            LLResult llResult = Limelight3A.getLatestResult();
 
-    @Override
-    public void start(){
-
-
-}
-    @Override
-    public void loop() {
-
+            if(llResult != null && llResult.isValid()){
+                Pose3D botPose = llResult.getBotpose_MT2();
+                telemetry.addData("Target x",llResult.getTx());
+                telemetry.addData("Target y", llResult.getTy());
+                telemetry.addData("Target area", llResult.getTa());
+                telemetry.addData("Yaw", botPose.getOrientation().getYaw());
+            }
+        }
     }
-}
